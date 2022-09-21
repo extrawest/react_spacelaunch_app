87306245df0dce57a177e7d16683319a35f64fbb
Vladyslav Haiduk <vladyslav.haiduk@extrawest.com>
2022-09-21 17:00:45 +0300

    feat: replace custom serviceWorker registrator with workbox-window

--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -1,4 +1,3 @@
 export { joinFonts } from './lib/joinFonts';
 export { pctToFloat } from './lib/pctToFloat';
 export { pxToRem } from './lib/pxToRem';
-export { registerSW } from './lib/registerSW';
