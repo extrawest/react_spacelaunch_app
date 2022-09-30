30d8d1948a4cc20a8e634d7c4a37a3eed4773afd
Vladyslav Haiduk <vladyslav.haiduk@extrawest.com>
2022-09-30 13:11:21 +0300

    fix: remove custom logic of timer and use plugin instead

--- libs/shared/hooks/src/index.ts
+++ libs/shared/hooks/src/index.ts
@@ -1,2 +1,1 @@
-export { useCurrentDate } from './lib/useCurrentDate';
 export { useResetPageScroll } from './lib/useResetPageScroll';
--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -2,6 +2,4 @@ export { joinFonts } from './lib/joinFonts';
 export { pctToFloat } from './lib/pctToFloat';
 export { pxToRem } from './lib/pxToRem';
 export { registerWorkboxServiceWorker } from './lib/registerWorkboxServiceWorker';
-export { compareDates } from './lib/compareDates';
-export { formatTimeNumber } from './lib/formatTimeNumber';
 export { getUrlSearchParam } from './lib/getUrlSearchParam';
