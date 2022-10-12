d42e2a878f9a72ea93b7fb15eb123af99eb5ea14
Vladyslav Haiduk <vladyslav.haiduk@extrawest.com>
2022-10-12 11:49:26 +0300

    refactor: rename .env.example to .env.dist and move base apu url into .env

--- libs/client-app/src/lib/store/api/api.ts
+++ libs/client-app/src/lib/store/api/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://spacelaunchnow.me/api/3.3.0/',
+    baseUrl: process.env.NX_BASE_API_URL,
   }),
   endpoints: () => ({}),
 });
