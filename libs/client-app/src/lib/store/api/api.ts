499c3f18047c797ec43ad1cdafd1c218f236a1d6
Vladyslav Haiduk <vladyslav.haiduk@extrawest.com>
2022-10-14 13:32:22 +0300

    fix: add log of process.env

--- libs/client-app/src/lib/store/api/api.ts
+++ libs/client-app/src/lib/store/api/api.ts
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+console.log(process.env);
+
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
